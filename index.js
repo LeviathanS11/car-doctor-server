d315852d1f7430bace2708d997442d47fc20dfa9
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-05-22 16:37:32 +0600

    server side working fine

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//carDoctor
+//SfAlJXlvakq3uhiX
 
 console.log(process.env.DB_PASS)
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wiqpkut.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,8 +30,8 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const serviceCollection = client.db('carDoctor').collection('services');
-        const bookingCollection = client.db('carDoctor').collection('bookings');
+        const serviceCollection = client.db('carDB').collection('services');
+        const bookingCollection = client.db('carDB').collection('bookings');
 
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
